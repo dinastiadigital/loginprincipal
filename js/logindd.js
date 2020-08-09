a3953e550ece2077799bb39d8f496da1f4331590
dinastiadigital <67850263+dinastiadigital@users.noreply.github.com>
2020-08-08 19:41:16 -0500

    Update logindd.js

--- js/logindd.js
+++ js/logindd.js
@@ -1,12 +1,6 @@
 function logindd(){
      var email = document.gerElementById('email').value;
      var pwd = document.getElementById('pwd').value;
-     
-     firebase.auth().signInWithEmailAndPassword(email, pwd).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // ...
-});
-     
+   
+     console.log("Hola");
 }
